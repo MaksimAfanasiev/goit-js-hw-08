4e711daf65b0a289d010cc2c3983968b182e80cd
MaksimAfanasiev <m_afanasyev@ukr.net>
2022-05-07 00:20:44 +0300

    Task 1
    Add Task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,25 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simpleLightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryEl = document.querySelector(".gallery")
+
+const galleryItemsMarkup = galleryItems.map(image =>
+    `
+    <a class="gallery__item" href="${image.original}">
+    <img class="gallery__image" src="${image.preview}" alt="${image.description}"/>
+    </a>
+    
+    `
+).join("");
+
+galleryEl.innerHTML = galleryItemsMarkup;
+
+let lightbox = new SimpleLightbox('.gallery a',
+    {captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,}
+);
+
