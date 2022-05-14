258e67d1ef2814f2339042b5c91399f9e03d27d5
MaksimAfanasiev <m_afanasyev@ukr.net>
2022-05-15 01:38:35 +0300

    BugFix
    Bug fix - Task2 and Task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,10 +3,17 @@ import _throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on("timeupdate", _throttle(onPlay, 1000));
+player.on('timeupdate', _throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
-    localStorage.setItem("videoplayer-current-time", seconds)
-};
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).catch(function (error) {
+  switch (error.name) {
+    case 'RangeError':
+      break;
+    default:
+      break;
+  }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,49 +1,47 @@
 import _throttle from 'lodash.throttle';
 
-const formEl = document.querySelector(".feedback-form");
+const formEl = document.querySelector('.feedback-form');
 const emailInpEl = document.querySelector('input[name="email"]');
 const messageInpEl = document.querySelector('textarea[name="message"]');
 
-const KEY = "feedback-form-state";
+const KEY = 'feedback-form-state';
 const dataObj = {};
 
+getStorageData();
 
-if (localStorage.getItem(KEY)) {
-    const storageData = JSON.parse(localStorage.getItem(KEY));
-    emailInpEl.value = storageData.email ? storageData.email : "";
-    messageInpEl.value = storageData.message ? storageData.message : "";
-}
-
-formEl.addEventListener("input", _throttle(onInputTyping, 500));
-
-formEl.addEventListener("submit", onFormSubmit);
+formEl.addEventListener('input', _throttle(onInputTyping, 500));
+formEl.addEventListener('submit', onFormSubmit);
 
 function onInputTyping(e) {
-    const inpName = e.target.name;
-    const inpValue = e.target.value;
+  const inpName = e.target.name;
+  const inpValue = e.target.value;
 
-    dataObj[inpName] = inpValue;
-    
-    const dataToSave = JSON.stringify(dataObj);
-    localStorage.setItem(KEY, dataToSave);
+  dataObj[inpName] = inpValue;
 
+  const dataToSave = JSON.stringify(dataObj);
+  localStorage.setItem(KEY, dataToSave);
 }
 
 function onFormSubmit(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const emailVal = e.currentTarget.elements.email.value;
-    const messageVal = e.currentTarget.elements.message.value;
+  console.log(dataObj);
 
-    const currentData = {
-        email: emailVal,
-        message: messageVal,
-    }
+  e.currentTarget.reset();
+  localStorage.removeItem(KEY);
 
-    console.log(currentData)
+  dataObj.email = '';
+  dataObj.message = '';
+}
 
-    emailInpEl.value = "";
-    messageInpEl.value = "";
+function getStorageData() {
+  if (localStorage.getItem(KEY)) {
+    const storageData = JSON.parse(localStorage.getItem(KEY));
 
-    localStorage.removeItem(KEY);
+    emailInpEl.value = storageData.email ? storageData.email : '';
+    messageInpEl.value = storageData.message ? storageData.message : '';
+
+    dataObj.email = emailInpEl.value;
+    dataObj.message = messageInpEl.value;
+  }
 }
